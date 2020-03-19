c84e5d1d69fb56877aed78b14df2df77c5ca2423
Ruan Evangelista Formigoni <ruanformigoni@gmail.com>
2020-03-19 17:24:04 -0300

    Moved install directives to conan

--- conanfile.py
+++ conanfile.py
@@ -19,16 +19,7 @@ class LorinaConan(ConanFile):
   def source(self):
     self.run("git clone git@github.com:hriener/lorina.git")
 
-    # Remove manually included deps
-    tools.replace_in_file(
-      "CMakeLists.txt",
-      "project(lorina LANGUAGES CXX)",
-      '''\nproject(lorina LANGUAGES CXX)\n'''
-      '''include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n'''
-      '''conan_basic_setup()''',
-      strict=True
-    )
-
+    # Remove manually inserted libraries
     tools.replace_in_file(
       "CMakeLists.txt",
       "add_subdirectory(lib)",
@@ -49,6 +40,49 @@ class LorinaConan(ConanFile):
       "include/lorina/diagnostics.hpp",
       "#include <rang/rang.hpp>",
       '#include <rang.hpp>',
+      strict=True
+    )
+    # Create install target
+    tools.replace_in_file(
+      "CMakeLists.txt",
+      '''if(LORINA_TEST)\n'''
+      '''  add_subdirectory(test)\n'''
+      '''endif()\n''',
+      '''# Install\n'''
+      '''include(GNUInstallDirs)\n\n'''
+      '''# Export targets\n'''
+      '''install(\n'''
+      '''  TARGETS lorina\n'''
+      '''  EXPORT lorina-targets\n'''
+      '''  DESTINATION ${CMAKE_INSTALL_LIBDIR}\n'''
+      ''')\n'''
+      '''# Install exported targets\n'''
+      '''install(EXPORT lorina-targets\n'''
+      '''  FILE lorina-targets.cmake\n'''
+      '''  NAMESPACE lorina::\n'''
+      '''  DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/\n'''
+      ''')\n'''
+      '''# Install lorina-config.cmake\n'''
+      '''install(\n'''
+      '''  FILES\n'''
+      '''    ${CMAKE_SOURCE_DIR}/cmake/lorina-config.cmake\n'''
+      '''  DESTINATION\n'''
+      '''    ${CMAKE_INSTALL_LIBDIR}/cmake/\n'''
+      ''')\n'''
+      '''# Install headers\n'''
+      '''install(\n'''
+      '''  DIRECTORY\n'''
+      '''    ${PROJECT_SOURCE_DIR}/include/lorina\n'''
+      '''  DESTINATION\n'''
+      '''    ${CMAKE_INSTALL_PREFIX}/include\n'''
+      ''')\n'''
+      '''# Install headers\n'''
+      '''install(\n'''
+      '''  DIRECTORY\n'''
+      '''    ${PROJECT_SOURCE_DIR}/include/lorina\n'''
+      '''  DESTINATION\n'''
+      '''    ${CMAKE_INSTALL_PREFIX}/include\n'''
+      ''')\n''',
       strict=True
     )
 
