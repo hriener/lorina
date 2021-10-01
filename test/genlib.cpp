579b41dc867e4537e061667a0b166606ff188367
aletempiac <alessandro.tempia@gmail.com>
2021-10-01 18:05:29 +0200

    Genlib: added output pin name to the data structure, support for multiline gate definition, support for Boolean expression with spaces

--- test/genlib.cpp
+++ test/genlib.cpp
@@ -10,6 +10,7 @@ struct gate
   std::string expression;
   double area;
   std::vector<pin_spec> pins;
+  std::string output_pin;
 };
 
 struct test_reader : public genlib_reader
@@ -19,9 +20,9 @@ public:
     : gates( gates )
   {}
 
-  void on_gate( std::string const& name, std::string const& expression, double area, std::vector<pin_spec> const& pins ) const
+  void on_gate( std::string const& name, std::string const& expression, double area, std::vector<pin_spec> const& pins, std::string const& output_pin ) const
   {
-    gates.emplace_back( gate{name, expression, area, pins} );
+    gates.emplace_back( gate{name, expression, area, pins, output_pin} );
   }
 
 public:
@@ -109,7 +110,7 @@ TEST_CASE( "read GENLIB format", "[genlib]")
     "GATE zero 0 O=0;\n"
     "GATE one 0	O=1;\n"
     "GATE inv1 1 O=!a; PIN * INV 1 999 1 1 1 1\n"
-    "GATE buf 2 O=a; PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
+    "GATE buf 2 Y=a; PIN * NONINV 1 999 1.0 1.0 1.0 1.0\n"
     ;
 
   text_diagnostics consumer;
@@ -124,27 +125,32 @@ TEST_CASE( "read GENLIB format", "[genlib]")
   CHECK( gate_definitions[0u].expression == "0" );
   CHECK( gate_definitions[0u].area == 0.0 );
   CHECK( gate_definitions[0u].pins.empty() );
+  CHECK( gate_definitions[0u].output_pin == "O" );
 
   CHECK( gate_definitions[1u].name == "one" );
   CHECK( gate_definitions[1u].expression == "1" );
   CHECK( gate_definitions[1u].area == 0.0 );
   CHECK( gate_definitions[1u].pins.empty() );
+  CHECK( gate_definitions[1u].output_pin == "O" );
 
   CHECK( gate_definitions[2u].name == "inv1" );
   CHECK( gate_definitions[2u].expression == "!a" );
   CHECK( gate_definitions[2u].area == 1.0 );
   CHECK( gate_definitions[2u].pins.size() == 1u );
+  CHECK( gate_definitions[2u].output_pin == "O" );
 
   CHECK( gate_definitions[3u].name == "buf" );
   CHECK( gate_definitions[3u].expression == "a" );
   CHECK( gate_definitions[3u].area == 2.0 );
   CHECK( gate_definitions[3u].pins.size() == 1u );
+  CHECK( gate_definitions[3u].output_pin == "Y" );
 }
 
 TEST_CASE( "PIN specification", "[genlib]")
 {
   std::string const genlib_file =
-    "GATE and2 1 O=a*b; PIN a INV 1.0 2.0 1.1 1.2 1.3 1.4 PIN b INV 1.0 2.0 1.0 1.0 1.0 1.0;\n"
+    "GATE and2 1 O=a*b; PIN a INV 1.0 2.0 1.1 1.2 1.3 1.4 \n"
+    "\tPIN b INV 1.0 2.0 1.0 1.0 1.0 1.0;\n"
     "GATE and3 1 O=a*b*c; PIN * UNKNOWN 1.0 2.0 1.0 1.0 1.0 1.0;\n"
     ;
 
